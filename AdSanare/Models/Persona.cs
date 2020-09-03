d704aea37128efdaa59aef13a802a9752587f5e6
Cristian Alan Gabito <gabo89ar@hotmail.com>
2020-09-03 09:43:24 -0300

    Inicio de proyecto

---
+++ AdSanare/Models/Persona.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AdSanare.Models
+{
+    public class Persona
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public string TipoDocumento { get; set; }
+        public string Documento { get; set; }
+        public DateTime FechaNacimiento { get; set; }
+        public int IdUsuario { get; set; }
+
+        // Trasa
+        public int IdUsuarioModificacion { get; set; }
+        public DateTime FechaAlta { get; set; }
+        public DateTime FechaModificacion { get; set; }
+    }
+}
--- README.md
+++
@@ -1,2 +0,0 @@
-# Gabito.Lescano
-AdSanare
