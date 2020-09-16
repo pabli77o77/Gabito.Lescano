f5ff553dc9f5045cf44b4f76faf4b84a47522065
Cristian Alan Gabito <gabo89ar@hotmail.com>
2020-09-16 13:09:28 -0300

    Testing Unitario

--- AdSanare.Logic.Tests/PersonaLogicTest.cs
+++ AdSanare.Logic.Tests/PersonaLogicTest.cs
@@ -1,8 +1,11 @@
 ﻿using AdSanare.Entities;
 using AdSanare.Logic.Interfaces;
 using AdSanare.Repository.Interfaces;
+using AdSanare.UOW.Interfaces;
 using AutoMoqCore;
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using Xunit;
 
 namespace AdSanare.Logic.Tests
@@ -15,7 +18,8 @@ namespace AdSanare.Logic.Tests
         public PersonaLogicTest()
         {
             _autoMoquer = new AutoMoqer();
-            _personaLogic = _autoMoquer.Resolve<PersonaLogic>();
+            _personaLogic = _autoMoquer.Resolve<PersonaLogic>();            
+            _autoMoquer.GetMock<IUnitOfWork>().Setup(m => m.Personas).Returns(_autoMoquer.GetMock<IPersonaRepository>().Object);
         }
 
         [Fact]
@@ -35,8 +39,7 @@ namespace AdSanare.Logic.Tests
                 FechaNacimiento = DateTime.Parse("1990-01-10")
             };
 
-            var mockPersonaRepository = _autoMoquer.GetMock<IPersonaRepository>();
-            mockPersonaRepository.Setup(p => p.Get(personaId)).Returns(persona);
+            _autoMoquer.GetMock<IPersonaRepository>().Setup(p => p.Get(personaId)).Returns(persona);
 
             var result = _personaLogic.Get(personaId);
 
@@ -50,6 +53,53 @@ namespace AdSanare.Logic.Tests
             Assert.Equal(persona.FechaNacimiento, result.FechaNacimiento);
             
         }
+        [Fact]
+        public void ObtieneListaPersonas()
+        {
+            List<Persona> listado = new List<Persona>()
+            {
+                new Persona
+                {
+                    Id = 1,
+                    Apellido = "Gomez",
+                    Nombre = "Jose",
+                    Documento = "46569878",
+                    ObraSocial = "OSDE",
+                    ObraSocialNumero = "6646464",
+                    Diagnostico = "Meningitis",
+                    FechaNacimiento = DateTime.Parse("1990-01-10")
+                },
+                new Persona
+                {
+                    Id = 2,
+                    Apellido = "Perez",
+                    Nombre = "Miguel",
+                    Documento = "32132123",
+                    ObraSocial = "OSDE",
+                    ObraSocialNumero = "99977784",
+                    Diagnostico = "Gripe",
+                    FechaNacimiento = DateTime.Parse("1970-01-10")
+                },
+                new Persona
+                {
+                    Id = 3,
+                    Apellido = "Lopez",
+                    Nombre = "Raul",
+                    Documento = "12345678",
+                    ObraSocial = "Swiss Medical",
+                    ObraSocialNumero = "232333",
+                    Diagnostico = "Gastroenteritis",
+                    FechaNacimiento = DateTime.Parse("1984-03-20")
+                }
 
+            };
+
+            _autoMoquer.GetMock<IPersonaRepository>().Setup(p => p.Get()).Returns(listado);
+
+            var result = _personaLogic.Get();
+
+            Assert.True(result != null);
+            Assert.Equal(listado.Count(), result.Count());
+        }
     }
 }
