ad181abb6d69c1f2120603795a438886c6f3759a
Cristian Alan Gabito <gabo89ar@hotmail.com>
2020-09-17 10:53:08 -0300

    Api Usuarios AdSanare

---
+++ AdSanare.ApiUsuarios/Controllers/RolesController.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AdSanare.ApiUsuarios.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace AdSanare.ApiUsuarios.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RolesController : ControllerBase
+    {
+        private readonly AdSanare_UsuariosContext _context;
+
+        public RolesController(AdSanare_UsuariosContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Rol>>> GetRoles()
+        {
+            return await _context.Roles.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Rol>> GetRole(short id)
+        {
+            var role = await _context.Roles.FindAsync(id);
+
+            if (role == null)
+            {
+                return NotFound();
+            }
+
+            return role;
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutRole(short id, Rol role)
+        {
+            if (id != role.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(role).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!RoleExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Rol>> PostRole(Rol role)
+        {
+            _context.Roles.Add(role);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetRole", new { id = role.Id }, role);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Rol>> DeleteRole(short id)
+        {
+            var role = await _context.Roles.FindAsync(id);
+            if (role == null)
+            {
+                return NotFound();
+            }
+
+            _context.Roles.Remove(role);
+            await _context.SaveChangesAsync();
+
+            return role;
+        }
+
+        private bool RoleExists(short id)
+        {
+            return _context.Roles.Any(e => e.Id == id);
+        }
+    }
+}
