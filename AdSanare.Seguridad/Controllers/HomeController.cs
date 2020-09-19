9eb249c1d166b5593a2aea34c57fef2d9139d91c
pablo <pablolescano87@hotmail.com>
2020-09-19 15:31:24 -0300

    Agregué el proyecto AdSanare.Seguridad con Identity

---
+++ AdSanare.Seguridad/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using AdSanare.Seguridad.Models;
+using AdSanare.MVC;
+
+namespace AdSanare.Seguridad.Controllers
+{
+    /// <summary>
+    /// Por ahora comento el Index para que no explote cuando levanta lod dos proyectos
+    /// </summary>
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        //public IActionResult Index()
+        //{
+        //    return View();
+        //}
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
