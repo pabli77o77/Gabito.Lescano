344bf081edbf05acea2691c82e05a62eb1ba782a
pablo <pablolescano87@hotmail.com>
2020-11-12 09:16:51 -0300

    Tests de Usuario, ObraSocial y Auditoria.

---
+++ AdSanare.Logic.Tests/AuditoriaLogicTest.cs
@@ -0,0 +1,110 @@
+﻿using AdSanare.Entities;
+using AdSanare.Logic.Interfaces;
+using AdSanare.Repository.Interfaces;
+using AdSanare.UOW.Interfaces;
+using AutoMoqCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+
+namespace AdSanare.Logic.Tests
+{
+    public class AuditoriaLogicTest
+    {
+        private readonly AutoMoqer _autoMoquer;
+        private readonly IAuditoriaLogic _auditoriaLogic;
+
+        public AuditoriaLogicTest()
+        {
+            _autoMoquer = new AutoMoqer();
+            _auditoriaLogic = _autoMoquer.Resolve<AuditoriaLogic>();
+            _autoMoquer.GetMock<IUserUnitOfWork>().Setup(a => a.Auditorias).Returns(_autoMoquer.GetMock<IAuditoriaRepository>().Object);
+        }
+
+        [Fact]
+        public void ObtenerAuditoriaPorId() 
+        {
+            Usuario usr = new Usuario
+            {
+                Name = "TEST",
+                LastName = "LastTest",
+                Id = "idMockUser",
+                UserName = "UserTest"
+            };
+
+            Auditoria auditoria = new Auditoria
+            { 
+                Id = 1,
+                Entidad = "Entidad",
+                EntidadId = 1,
+                TipoEntidad = "TipoEntidad",
+                FechaRegistro = DateTime.Now,
+                Usuario = usr
+            };
+
+            Auditoria aud1 = auditoria;
+            _autoMoquer.GetMock<IAuditoriaRepository>().Setup(i => i.Get(auditoria.Id)).Returns(aud1);
+
+            var result = _auditoriaLogic.Get(auditoria.Id);
+            Assert.Equal(auditoria.Id, result.Id);
+            Assert.Equal(auditoria.Entidad, result.Entidad);
+            Assert.Equal(auditoria.EntidadId, result.EntidadId);
+            Assert.Equal(auditoria.FechaRegistro, result.FechaRegistro);
+            Assert.Equal(auditoria.TipoEntidad, result.TipoEntidad);
+            Assert.Equal(auditoria.Usuario, result.Usuario);
+        }
+
+        [Fact]
+        public void ObtenerListAuditorias()
+        {
+            Usuario usr = new Usuario
+            {
+                Name = "TEST",
+                LastName = "LastTest",
+                Id = "idMockUser",
+                UserName = "UserTest"
+            };
+
+            List<Auditoria> listaAuditorias = new List<Auditoria>()
+            {
+                new Auditoria
+                {
+                    Id = 1,
+                    Entidad = "Entidad",
+                    EntidadId = 1,
+                    TipoEntidad = "TipoEntidad",
+                    FechaRegistro = DateTime.Now,
+                    Usuario = usr
+                },
+
+                new Auditoria
+                {
+                    Id = 2,
+                    Entidad = "Entidad_2",
+                    EntidadId = 2,
+                    TipoEntidad = "TipoEntidad_2",
+                    FechaRegistro = DateTime.Now,
+                    Usuario = usr
+                },
+
+                new Auditoria
+                {
+                    Id = 3,
+                    Entidad = "Entidad_3",
+                    EntidadId = 3,
+                    TipoEntidad = "TipoEntidad_3",
+                    FechaRegistro = DateTime.Now,
+                    Usuario = usr
+                }
+            };
+            _autoMoquer.GetMock<IAuditoriaRepository>().Setup(i => i.Get()).Returns(listaAuditorias);
+            var result = _auditoriaLogic.Get();
+
+            Assert.True(result != null);
+            Assert.Equal(listaAuditorias.Count, result.Count());
+        }
+
+    }
+}
---
+++ AdSanare.Logic.Tests/UsuarioLogicTest.cs
@@ -0,0 +1,81 @@
+﻿using AdSanare.Entities;
+using AdSanare.Logic.Interfaces;
+using AdSanare.Repository.Interfaces;
+using AdSanare.UOW.Interfaces;
+using AutoMoqCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+
+namespace AdSanare.Logic.Tests
+{
+    public class UsuarioLogicTest
+    {
+        private readonly AutoMoqer _autoMoquer;
+        private readonly IUsuarioLogic _usuarioLogic;
+
+        public UsuarioLogicTest()
+        {
+            _autoMoquer = new AutoMoqer();
+            _usuarioLogic = _autoMoquer.Resolve<UsuarioLogic>();
+            _autoMoquer.GetMock<IUserUnitOfWork>().Setup(u => u.Usuarios).Returns(_autoMoquer.GetMock<IUsuarioRepository>().Object);
+        }
+
+        [Fact]
+        public void ObtenerUsuarioPorId()
+        {
+            Usuario usr = new Usuario
+            {
+                Name = "TEST",
+                LastName = "LastTest",
+                Id = "idMockUser",
+                UserName = "UserTest"
+            };
+
+            Usuario user = usr;
+            _autoMoquer.GetMock<IUsuarioRepository>().Setup(u => u.Get(usr.Id)).Returns(user);
+
+            var result = _usuarioLogic.Get(usr.Id);
+            Assert.Equal(usr.Id, result.Id);
+        }
+
+        [Fact]
+        public void ObtenerListaUsuarios()
+        {
+            List<Usuario> listaUsuarios = new List<Usuario>()
+            {
+                new Usuario
+                {
+                    Name = "TEST",
+                    LastName = "LastTest",
+                    Id = "idMockUser",
+                    UserName = "UserTest"
+                },
+
+                new Usuario
+                {
+                    Name = "TEST_1",
+                    LastName = "LastTest_1",
+                    Id = "idMockUser_1",
+                    UserName = "UserTest_1"
+                },
+
+                new Usuario
+                {
+                    Name = "TEST_2",
+                    LastName = "LastTest_2",
+                    Id = "idMockUser_2",
+                    UserName = "UserTest_2"
+                }
+            };
+
+            _autoMoquer.GetMock<IUsuarioRepository>().Setup(o => o.Get()).Returns(listaUsuarios);
+            var result = _usuarioLogic.Get();
+
+            Assert.True(result != null);
+            Assert.Equal(listaUsuarios.Count, result.Count());
+        }
+    }
+}
